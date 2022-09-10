b728435da9faa5fdc7d3487b442ea038b868232c
Irena Grgic <irena.grgic@zeiss.com>
2022-09-10 22:46:06 +0200

    Moved parts from main into a separate draw function in the mondrian package

--- main.go
+++ main.go
@@ -1,84 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"image"
 	"image/color"
-	"image/draw"
-	"image/png"
-	"log"
-	"math/rand"
 	"mondrian/mondrian"
-	"os"
-	"time"
 )
 
-var colors = []color.Color{
-	color.White,
-	// red
-	color.RGBA{255, 0, 0, 255},
-	// yellow
-	color.RGBA{255, 255, 0, 255},
-	// blue
-	color.RGBA{0, 0, 255, 255},
-}
-
 func main() {
+	// Define canvas size
 	x0 := 0
 	x1 := 150
 	y0 := 0
 	y1 := 150
-	lambda := float64(x1 - x0 + y1 - y0)
-	// Initializing the Mondrian Process
-	mp := mondrian.MProcess{
-		Lambda: lambda,
-		X0:     x0,
-		X1:     x1,
-		Y0:     y0,
-		Y1:     y1,
+	// Complexity of the partitioning: the lower the number, the less partitions are made.
+	//Do not input a number larger than 0.08 for the 150x150 canvas
+	//If choosing the onlyRect option (draws only rectangles without the lines, go for
+	//higher complexity, e.g. 0.05).
+	cmplx := 0.03
+
+	// Draws rectangles and lines
+	mondrian.Draw(x0, x1, y0, y1, cmplx)
+
+	// Draws only rectangles with higher complexity and given colors and color distribution
+	var cols = []color.Color{
+		color.White,
+		color.RGBA{255, 0, 0, 255},
+		color.RGBA{255, 255, 0, 255},
+		color.RGBA{0, 0, 255, 255},
 	}
-	// Initializing the Rectangles slice
-	r := []image.Rectangle{
-		{Min: image.Pt(mp.X0, mp.Y0), Max: image.Pt(mp.X1, mp.Y1)},
+	var probs = []float64{
+		0.3,
+		0.4,
+		0.3,
+		0.3,
 	}
-
-	pr := mondrian.MondrianProcess(mp, &r, true, 0.04)
-
-	rectImage := image.NewRGBA((*pr)[0])
-
-	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
-
-	// Drawing rectangle
-	for inx, rect := range (*pr)[1:] {
-		c := mondrian.RandomColor(colors[0], colors[1:])
-		fmt.Println(inx, rect, c)
-		draw.Draw(rectImage, rect, &image.Uniform{c}, image.ZP, draw.Src)
-	}
-
-	file, err := os.Create("img/Rectangles.png")
-	if err != nil {
-		log.Fatalf("failed create file: %s", err)
-	}
-	png.Encode(file, rectImage)
-
-	// Drawing lines
-	lineImage := image.NewRGBA(image.Rectangle{image.Point{x0, y0}, image.Point{x1, y1}})
-
-	mondrian.DrawLines(pr, lineImage, color.Black)
-
-	// Encode as PNG.
-	f, err := os.Create("img/Lines.png")
-	if err != nil {
-		log.Fatalf("failed create file: %s", err)
-	}
-	png.Encode(f, lineImage)
-
-	// Overlaying the two images
-	draw.Draw(rectImage, r[0], lineImage, image.ZP, draw.Over)
-
-	f, err = os.Create("img/Overlay.png")
-	if err != nil {
-		log.Fatalf("failed create file: %s", err)
-	}
-	png.Encode(f, rectImage)
+	mondrian.DrawR(x0, x1, y0, y1, 0.07, true, cols, probs)
 }
---
+++ mondrian/draw.go
@@ -0,0 +1,94 @@
+package mondrian
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+// Draw calls DrawR without the additional parameters onlyRect, c and p
+// and thus draws both the Rectangles and Lines on the same initial canvas
+// under given complexity cmplx.
+func Draw(
+	x0 int,
+	x1 int,
+	y0 int,
+	y1 int,
+	cmplx float64,
+) {
+	DrawR(x0, x1, y0, y1, cmplx, false, Colors, ColorProbs)
+}
+
+// DrawR takes a given canvas size and calls functions Rectangles and Lines to generate
+// a single image given the partition complexity cmplx. If onlyRect is true, then the image
+// is generated only with Rectangles. The colors of the partitions are set in cols and the
+// distribution of colors in probs.
+func DrawR(
+	x0 int,
+	x1 int,
+	y0 int,
+	y1 int,
+	cmplx float64,
+	onlyRect bool,
+	cols []color.Color,
+	probs []float64,
+) {
+	// Initialize global pseudo random generator
+	rand.Seed(time.Now().Unix())
+
+	// Calculating the partitioning budget
+	lambda := float64(x1 - x0 + y1 - y0)
+
+	// Initializing the Mondrian Process
+	mp := MProcess{
+		Lambda: lambda,
+		X0:     x0,
+		X1:     x1,
+		Y0:     y0,
+		Y1:     y1,
+	}
+	// Initializing the first rectangle in canvas size
+	r := []image.Rectangle{
+		{Min: image.Pt(mp.X0, mp.Y0), Max: image.Pt(mp.X1, mp.Y1)},
+	}
+	pr := Rectangles(mp, &r, true, cmplx)
+
+	// Generating the empty canvas for the drawing of rectangles
+	rectImage := image.NewRGBA((*pr)[0])
+
+	// Drawing rectangles
+	for inx, rect := range (*pr)[1:] {
+		c := RndColor(cols, probs)
+		fmt.Println(inx, rect, c)
+		draw.Draw(rectImage, rect, &image.Uniform{c}, image.ZP, draw.Src)
+	}
+
+	if onlyRect {
+		f, err := os.Create("img/Rectangles.png")
+		if err != nil {
+			log.Fatalf("failed create file: %s", err)
+		}
+		png.Encode(f, rectImage)
+		return
+	}
+
+	// Drawing lines
+	lineImage := image.NewRGBA(image.Rectangle{image.Point{x0, y0}, image.Point{x1, y1}})
+
+	Lines(pr, lineImage, color.Black)
+
+	// Overlaying the two images
+	draw.Draw(rectImage, r[0], lineImage, image.ZP, draw.Over)
+
+	f, err := os.Create("img/RectandLines.png")
+	if err != nil {
+		log.Fatalf("failed create file: %s", err)
+	}
+	png.Encode(f, rectImage)
+}
