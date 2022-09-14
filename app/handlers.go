80b0a4679603aadf60a51c25e8a77eace163ebf9
balu <korzinovvv@gmail.com>
2022-09-14 22:41:18 +0200

    user choices stay unchanged after submitting a form

--- app/handlers.go
+++ app/handlers.go
@@ -8,6 +8,12 @@ import (
 	"strconv"
 )
 
+type PageData struct {
+	Complexity    float64
+	IsRectandLine bool
+	IsRect        bool
+}
+
 // imageHandler responds to an incoming request by generating a Mondrian image fo size 500x500
 // and according to the chosen style and complexity which is taken from the request form. The
 // response is a html block with the image source and dimensions.
@@ -16,11 +22,18 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	x1 := 500
 	y0 := 0
 	y1 := 500
+	complexity := 0.01
 	image_location := "img/mondrian_image.png"
+	templatedata := PageData{
+		Complexity:    complexity,
+		IsRectandLine: false,
+		IsRect:        true,
+	}
 
 	if r.Method == "GET" {
 		// render a default image
-		mondrian.Draw(x0, x1, y0, y1, 0.01, image_location)
+		mondrian.Draw(x0, x1, y0, y1, complexity, image_location)
+
 	} else {
 		err := r.ParseForm()
 		if err != nil {
@@ -30,10 +43,16 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		cmplx, _ := strconv.ParseFloat(r.FormValue("complexity"), 64)
+		complexity = cmplx // overwriting to pass later to the template
 		style := r.FormValue("style")
 
 		if style == "rectandline" {
 			mondrian.Draw(x0, x1, y0, y1, cmplx, image_location)
+			templatedata = PageData{
+				Complexity:    complexity,
+				IsRectandLine: true,
+				IsRect:        false,
+			}
 		} else if style == "rect" {
 			var cols = []color.Color{
 				color.White,
@@ -48,6 +67,11 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 				0.3,
 			}
 			mondrian.DrawR(x0, x1, y0, y1, cmplx, true, cols, probs, image_location)
+			templatedata = PageData{
+				Complexity:    complexity,
+				IsRectandLine: false,
+				IsRect:        true,
+			}
 		} else {
 			fmt.Println(fmt.Errorf("error: style %s not allowed", style))
 			w.WriteHeader(http.StatusInternalServerError)
@@ -55,5 +79,5 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 
 	}
 
-	tpl.Execute(w, nil)
+	tpl.Execute(w, templatedata)
 }
